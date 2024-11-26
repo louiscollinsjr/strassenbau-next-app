2c5c3f4f4d008855b5489d418a3346e1800a6bc7
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-26 05:54:43 +0200

    reponsive changes..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,21 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <div className="mx-auto px-4 py-12 md:py-20 min-h-[50vh] flex flex-col justify-center" style={{ maxWidth: '980px' }}>
+      <div className="mx-auto px-4 py-8 md:py-20 min-h-[50vh] flex flex-col justify-center" style={{ maxWidth: '980px' }}>
         <div className="flex justify-left mb-2 pl-1">
           <Image
             src="/images/logo_bridge.svg"
             alt="Bridge Logo"
-            width={120}
-            height={120}
+            width={100}
+            height={100}
+            className="w-[80px] h-[80px] md:w-[100px] md:h-[100px]"
             priority
           />
         </div>
-        <h1 className="text-xl md:text-xs font-ligt mb-3 md:mb-4">Strassen und Brückenbau</h1>
-        <p className="text-gray-600 md:text-7xl font-bold">Building Connections, Strengthening Communities.</p>
+        <h1 className="text-sm md:text-xl lg:text-2xl font-light mb-2 md:mb-4">Strassen und Brückenbau</h1>
+        <p className="text-3xl md:text-5xl lg:text-7xl font-bold text-gray-600">Building Connections, Strengthening Communities.</p>
       </div>
-      <div className="mx-auto pt-24" style={{ maxWidth: '980px' }}></div>
+      <div className="mx-auto pt-12 md:pt-24" style={{ maxWidth: '980px' }}></div>
       <ThermalRehabProjects />
       <div className="mx-auto border-b-4 border-gray-200" style={{ maxWidth: '980px' }}></div>
       <CollegeProjects />
