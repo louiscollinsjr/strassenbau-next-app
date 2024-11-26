57c9c20854e2386024c994cef152e161300389e4
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-26 16:48:25 +0200

    updated header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Header() {
     return (
         <header className="fixed w-full bg-[rgb(29,30,32)]/90 backdrop-blur-sm z-50 border-b border-white/0">
-            <div className="max-w-5xl mx-auto px-8 py-4">
+            <div className="max-w-5xl mx-auto px-8 py-6">
                 <div className="flex items-center justify-between">
                     <Link href="/" className="text-lg md:text-4xl font-semibold text-white">
                         Strassen und Brückenbau
