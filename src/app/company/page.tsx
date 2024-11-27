49d6c03d1b358f4f714671ec79a68f41ca32742d
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-27 12:13:41 +0200

    update company hero

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -2,7 +2,6 @@
 
 import CompanyPartners from "../components/CompanyPartners";
 import Clients from "../components/Clients";
-import CompanyHero from '../components/CompanyHero';
 import CompanySpecializations from "../components/CompanySpecializations";
 import CompanyWelcome from "../components/CompanyWelcome";
 import CompanyHeroPlus from "../components/CompanyHeroPlus";
@@ -11,7 +10,6 @@ export default function CompanyPage() {
     return (
         <main className="md:pt-32">
             <CompanyHeroPlus />
-            <CompanyHero />
             <CompanyWelcome />
             <CompanySpecializations />
             <Clients />
