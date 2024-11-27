b7d42dd69b342e4643954e7f39a2fc673c753b4d
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-27 10:20:58 +0200

    fixed theme in navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { ThemeToggle } from './theme-toggle';
+// import { ThemeToggle } from './theme-toggle';
 
 export default function Navigation() {
   return (
