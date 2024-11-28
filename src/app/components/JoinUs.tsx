1eefb5e11ae2fa06925edf98968ad2986cccf289
louiscollinsjr <louiscollinsjr@gmail.com>
2024-11-28 13:57:02 +0200

    fixed join

--- src/app/components/JoinUs.tsx
+++ src/app/components/JoinUs.tsx
@@ -53,7 +53,7 @@ export default function JoinUs() {
             >
               View Open Positions
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="ml-2 w-4 h-4">
-              <path strokeLineCap="round" strokeLineJoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+              <path strokeLineCap="round" strokeLineJoin="round" d="M4.5 19.5 15 4.5M8.25 4.5l11.25 11.25" />
               </svg>
 
             </Link>
